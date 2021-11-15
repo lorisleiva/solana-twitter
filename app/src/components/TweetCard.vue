32cd99cc13111521025edf07c115aaad80ef28c2
Loris Leiva <loris.leiva@gmail.com>
2021-11-15 14:08:58 +0000

    Add URLs

--- app/src/components/TweetCard.vue
+++ app/src/components/TweetCard.vue
@@ -11,13 +11,17 @@ const { tweet } = toRefs(props)
 <template>
     <div class="px-8 py-4">
         <div>
-            <h3 class="inline font-semibold" v-text="tweet.author_display" :title="tweet.author"></h3>
+            <h3 class="inline font-semibold" :title="tweet.author">
+                <router-link :to="{ name: 'Users', params: { author: tweet.author.toBase58() } }" class="hover:underline">
+                    {{ tweet.author_display }}
+                </router-link>
+            </h3>
             <span class="text-gray-500"> • </span>
             <time class="text-gray-500 text-sm" v-text="tweet.created_ago" :title="tweet.created_at"></time>
         </div>
         <p class="whitespace-pre-wrap" v-text="tweet.content"></p>
-        <a href="#" v-if="tweet.topic" class="inline-block mt-2 text-pink-500 hover:underline">
+        <router-link v-if="tweet.topic" :to="{ name: 'Topics', params: { topic: tweet.topic } }" class="inline-block mt-2 text-pink-500 hover:underline">
             #{{ tweet.topic }}
-        </a>
+        </router-link>
     </div>
 </template>
