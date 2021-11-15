33523d8ac686840b7d6becf46b016be757dbd83f
Loris Leiva <loris.leiva@gmail.com>
2021-11-15 14:03:49 +0000

    wip

--- app/src/components/TheSidebar.vue
+++ app/src/components/TheSidebar.vue
@@ -11,7 +11,7 @@ import { WalletMultiButton, WalletModalProvider } from '@solana/wallet-adapter-v
             </svg>
         </router-link>
         <div class="space-y-2">
-            <router-link to="/" class="rounded-full hover:bg-gray-100 p-3 w-full flex items-center space-x-4" active-class="font-bold" v-slot="{ isActive }">
+            <router-link :to="{ name: 'Home' }" class="rounded-full hover:bg-gray-100 p-3 w-full flex items-center space-x-4" active-class="font-bold" v-slot="{ isActive }">
                 <svg v-if="isActive" xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-gray-700" viewBox="0 0 20 20" fill="currentColor">
                     <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
                 </svg>
@@ -20,7 +20,7 @@ import { WalletMultiButton, WalletModalProvider } from '@solana/wallet-adapter-v
                 </svg>
                 <div class="text-xl">Home</div>
             </router-link>
-            <router-link to="/topics" class="rounded-full hover:bg-gray-100 p-3 w-full flex items-center space-x-4" active-class="font-bold" v-slot="{ isActive }">
+            <router-link :to="{ name: 'Topics' }" class="rounded-full hover:bg-gray-100 p-3 w-full flex items-center space-x-4" active-class="font-bold" v-slot="{ isActive }">
                 <svg v-if="isActive" xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-gray-700" viewBox="0 0 20 20" fill="currentColor">
                     <path d="M2 5a2 2 0 012-2h7a2 2 0 012 2v4a2 2 0 01-2 2H9l-3 3v-3H4a2 2 0 01-2-2V5z" />
                     <path d="M15 7v2a4 4 0 01-4 4H9.828l-1.766 1.767c.28.149.599.233.938.233h2l3 3v-3h2a2 2 0 002-2V9a2 2 0 00-2-2h-1z" />
@@ -30,7 +30,7 @@ import { WalletMultiButton, WalletModalProvider } from '@solana/wallet-adapter-v
                 </svg>
                 <div class="text-xl">Topics</div>
             </router-link>
-            <router-link to="/users" class="rounded-full hover:bg-gray-100 p-3 w-full flex items-center space-x-4" active-class="font-bold" v-slot="{ isActive }">
+            <router-link :to="{ name: 'Users' }" class="rounded-full hover:bg-gray-100 p-3 w-full flex items-center space-x-4" active-class="font-bold" v-slot="{ isActive }">
                 <svg v-if="isActive" xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-gray-700" viewBox="0 0 20 20" fill="currentColor">
                     <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
                 </svg>
@@ -39,7 +39,7 @@ import { WalletMultiButton, WalletModalProvider } from '@solana/wallet-adapter-v
                 </svg>
                 <div class="text-xl">Users</div>
             </router-link>
-            <router-link to="/profile" class="rounded-full hover:bg-gray-100 p-3 w-full flex items-center space-x-4" active-class="font-bold" v-slot="{ isActive }">
+            <router-link :to="{ name: 'Profile' }" class="rounded-full hover:bg-gray-100 p-3 w-full flex items-center space-x-4" active-class="font-bold" v-slot="{ isActive }">
                 <svg v-if="isActive" xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-gray-700" viewBox="0 0 20 20" fill="currentColor">
                     <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
                 </svg>
--- app/src/composables/useSlug.js
+++ app/src/composables/useSlug.js
@@ -2,7 +2,7 @@ import { computed } from 'vue'
 
 export const useSlug = text => {
     return computed(() => {
-        return text.value
+        return (text.value || '')
             .toLowerCase()
             .replace(/[^a-z0-9 -]/g, '')
             .replace(/\s+/g, '-')
