4f4712da869dbccc058605d2410a1d1e3926f803
Loris Leiva <loris.leiva@gmail.com>
2021-11-24 10:20:23 +0000

    Delete dummy test and reindent

--- tests/solana-twitter.ts
+++ tests/solana-twitter.ts
@@ -3,15 +3,7 @@ import { Program } from '@project-serum/anchor';
 import { SolanaTwitter } from '../target/types/solana_twitter';
 
 describe('solana-twitter', () => {
-
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
-
-  const program = anchor.workspace.SolanaTwitter as Program<SolanaTwitter>;
-
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
-  });
+    // Configure the client to use the local cluster.
+    anchor.setProvider(anchor.Provider.env());
+    const program = anchor.workspace.SolanaTwitter as Program<SolanaTwitter>;
 });
