140d122e074a82918b5a782b95449dc91ce25130
Loris Leiva <loris.leiva@gmail.com>
2021-11-24 13:22:38 +0000

    Test our program can send a new tweet

--- tests/solana-twitter.ts
+++ tests/solana-twitter.ts
@@ -1,9 +1,32 @@
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
 import { SolanaTwitter } from '../target/types/solana_twitter';
+import * as assert from "assert";
 
 describe('solana-twitter', () => {
     // Configure the client to use the local cluster.
     anchor.setProvider(anchor.Provider.env());
     const program = anchor.workspace.SolanaTwitter as Program<SolanaTwitter>;
+
+    it('can send a new tweet', async () => {
+        // Call the "SendTweet" instruction.
+        const tweet = anchor.web3.Keypair.generate();
+        await program.rpc.sendTweet('veganism', 'Hummus, am I right?', {
+            accounts: {
+                tweet: tweet.publicKey,
+                author: program.provider.wallet.publicKey,
+                systemProgram: anchor.web3.SystemProgram.programId,
+            },
+            signers: [tweet],
+        });
+
+        // Fetch the account details of the created tweet.
+        const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
+
+        // Ensure it has the right data.
+        assert.equal(tweetAccount.author.toBase58(), program.provider.wallet.publicKey.toBase58());
+        assert.equal(tweetAccount.topic, 'veganism');
+        assert.equal(tweetAccount.content, 'Hummus, am I right?');
+        assert.ok(tweetAccount.timestamp);
+    });
 });
