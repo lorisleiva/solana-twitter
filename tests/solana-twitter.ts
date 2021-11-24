2c2171b5bb962c5e7d0a8b49d3a00c1f404d0f03
Loris Leiva <loris.leiva@gmail.com>
2021-11-24 15:19:20 +0000

    Test custom guards

--- tests/solana-twitter.ts
+++ tests/solana-twitter.ts
@@ -78,4 +78,44 @@ describe('solana-twitter', () => {
         assert.equal(tweetAccount.content, 'Yay Tofu!');
         assert.ok(tweetAccount.timestamp);
     });
+
+    it('cannot provide a topic with more than 50 characters', async () => {
+        try {
+            const tweet = anchor.web3.Keypair.generate();
+            const topicWith51Chars = 'x'.repeat(51);
+            await program.rpc.sendTweet(topicWith51Chars, 'Hummus, am I right?', {
+                accounts: {
+                    tweet: tweet.publicKey,
+                    author: program.provider.wallet.publicKey,
+                    systemProgram: anchor.web3.SystemProgram.programId,
+                },
+                signers: [tweet],
+            });
+        } catch (error) {
+            assert.equal(error.msg, 'The provided topic should be 50 characters long maximum.');
+            return;
+        }
+
+        assert.fail('The instruction should have failed with a 51-character topic.');
+    });
+
+    it('cannot provide a content with more than 280 characters', async () => {
+        try {
+            const tweet = anchor.web3.Keypair.generate();
+            const contentWith281Chars = 'x'.repeat(281);
+            await program.rpc.sendTweet('veganism', contentWith281Chars, {
+                accounts: {
+                    tweet: tweet.publicKey,
+                    author: program.provider.wallet.publicKey,
+                    systemProgram: anchor.web3.SystemProgram.programId,
+                },
+                signers: [tweet],
+            });
+        } catch (error) {
+            assert.equal(error.msg, 'The provided content should be 280 characters long maximum.');
+            return;
+        }
+
+        assert.fail('The instruction should have failed with a 281-character content.');
+    });
 });
