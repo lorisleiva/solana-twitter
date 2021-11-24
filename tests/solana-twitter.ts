968b812e56b686cb7ed1de9f444cedab4c1bc454
Loris Leiva <loris.leiva@gmail.com>
2021-11-24 13:35:01 +0000

    Test sending tweets without topics

--- tests/solana-twitter.ts
+++ tests/solana-twitter.ts
@@ -29,4 +29,26 @@ describe('solana-twitter', () => {
         assert.equal(tweetAccount.content, 'Hummus, am I right?');
         assert.ok(tweetAccount.timestamp);
     });
+
+    it('can send a new tweet without a topic', async () => {
+        // Call the "SendTweet" instruction.
+        const tweet = anchor.web3.Keypair.generate();
+        await program.rpc.sendTweet('', 'gm', {
+            accounts: {
+                tweet: tweet.publicKey,
+                author: program.provider.wallet.publicKey,
+                systemProgram: anchor.web3.SystemProgram.programId,
+            },
+            signers: [tweet],
+        });
+
+        // Fetch the account details of the created tweet.
+        const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
+
+        // Ensure it has the right data.
+        assert.equal(tweetAccount.author.toBase58(), program.provider.wallet.publicKey.toBase58());
+        assert.equal(tweetAccount.topic, '');
+        assert.equal(tweetAccount.content, 'gm');
+        assert.ok(tweetAccount.timestamp);
+    });
 });
