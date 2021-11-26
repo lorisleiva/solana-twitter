a3c08786c6f4084d6e9950320a50e3f85e14f324
Loris Leiva <loris.leiva@gmail.com>
2021-11-26 11:33:49 +0000

    Configure node polyfills

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,4 +1,24 @@
+const webpack = require('webpack')
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
+    configureWebpack: {
+        plugins: [
+            new webpack.ProvidePlugin({
+                Buffer: ['buffer', 'Buffer']
+            })
+        ],
+        resolve: {
+            fallback: {
+                crypto: false,
+                fs: false,
+                assert: false,
+                process: false,
+                util: false,
+                path: false,
+                stream: false,
+            }
+        }
+    }
 })
