d801758a2e75097075fe7d9eb18b9881758fb5ec
Loris Leiva <loris.leiva@gmail.com>
2021-11-26 12:23:24 +0000

    Install Vue Router

--- app/src/main.js
+++ app/src/main.js
@@ -1,7 +1,15 @@
 // CSS.
 import './main.css'
 
+// Routing.
+import { createRouter, createWebHashHistory } from 'vue-router'
+import routes from './routes'
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+})
+
 // Create the app.
 import { createApp } from 'vue'
 import App from './App.vue'
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
