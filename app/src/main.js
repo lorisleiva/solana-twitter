a988cdbdf5f430f1561e346884ff41fedd737bc4
Loris Leiva <loris.leiva@gmail.com>
2021-11-26 12:05:12 +0000

    Install TailwindCSS

--- app/src/main.js
+++ app/src/main.js
@@ -1,4 +1,7 @@
+// CSS.
+import './main.css'
+
+// Create the app.
 import { createApp } from 'vue'
 import App from './App.vue'
-
 createApp(App).mount('#app')
---
+++ app/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './public/index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
