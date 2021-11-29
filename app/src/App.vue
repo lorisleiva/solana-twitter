3c93d0ec50997a3b388f3baf5c2ffb28c2f83728
Loris Leiva <loris.leiva@gmail.com>
2021-11-29 17:04:49 +0000

    Use the wallet provider

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,22 +1,31 @@
 <script setup>
 import { useRoute } from 'vue-router'
 import TheSidebar from './components/TheSidebar'
+import { getPhantomWallet, getSolflareWallet } from '@solana/wallet-adapter-wallets'
+import { WalletProvider } from '@solana/wallet-adapter-vue'
 
 const route = useRoute()
+
+const wallets = [
+    getPhantomWallet(),
+    getSolflareWallet(),
+]
 </script>
 
 <template>
-    <div class="w-full max-w-3xl lg:max-w-4xl mx-auto">
+    <wallet-provider :wallets="wallets" auto-connect>
+        <div class="w-full max-w-3xl lg:max-w-4xl mx-auto">
 
-        <!-- Sidebar. -->
-        <the-sidebar class="py-4 md:py-8 md:pl-4 md:pr-8 fixed w-20 md:w-64"></the-sidebar>
+            <!-- Sidebar. -->
+            <the-sidebar class="py-4 md:py-8 md:pl-4 md:pr-8 fixed w-20 md:w-64"></the-sidebar>
 
-        <!-- Main -->
-        <main class="flex-1 border-r border-l ml-20 md:ml-64 min-h-screen">
-            <header class="flex space-x-6 items-center justify-between px-8 py-4 border-b">
-                <div class="text-xl font-bold" v-text="route.name"></div>
-            </header>
-            <router-view></router-view>
-        </main>
-    </div>
+            <!-- Main -->
+            <main class="flex-1 border-r border-l ml-20 md:ml-64 min-h-screen">
+                <header class="flex space-x-6 items-center justify-between px-8 py-4 border-b">
+                    <div class="text-xl font-bold" v-text="route.name"></div>
+                </header>
+                <router-view></router-view>
+            </main>
+        </div>
+    </wallet-provider>
 </template>
