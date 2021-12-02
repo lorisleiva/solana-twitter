9712ff84613ae95c01da3ee9827d8162f0f8129a
Loris Leiva <loris.leiva@gmail.com>
2021-12-02 14:44:19 +0000

    Supporting filters

--- app/src/api/fetch-tweets.js
+++ app/src/api/fetch-tweets.js
@@ -1,6 +1,24 @@
-import { Tweet } from '@/models';
+import { Tweet } from '@/models'
+import bs58 from 'bs58'
 
-export const fetchTweets = async ({ program }) => {
-    const tweets = await program.value.account.tweet.all();
+export const fetchTweets = async ({ program }, filters = []) => {
+    const tweets = await program.value.account.tweet.all(filters);
     return tweets.map(tweet => new Tweet(tweet.publicKey, tweet.account))
 }
+
+export const authorFilter = authorBase58PublicKey => ({
+    memcmp: {
+        offset: 8, // Discriminator.
+        bytes: authorBase58PublicKey,
+    }
+})
+
+export const topicFilter = topic => ({
+    memcmp: {
+        offset: 8 + // Discriminator.
+            32 + // Author public key.
+            8 + // Timestamp.
+            4, // Topic string prefix.
+        bytes: bs58.encode(Buffer.from(topic)),
+    }
+})
