2aa50a93f19fe2715f0ad185c627bfc81b2b4fe9
Loris Leiva <loris.leiva@gmail.com>
2021-12-02 12:06:35 +0000

    Fetching all tweets

--- app/src/api/fetch-tweets.js
+++ app/src/api/fetch-tweets.js
@@ -1,28 +1,7 @@
+import { useWorkspace } from '@/composables'
+
 export const fetchTweets = async () => {
-    return [
-        {
-            topic: 'solana',
-            content: 'gm',
-            author_display: 'B1Af..wtRN',
-            created_at: 'Nov 26, 2021 1:03PM',
-            created_ago: 'just now',
-            timestamp: 1637932864,
-        },
-        {
-            topic: 'no-code',
-            content: 'Octohook.com is awesome!',
-            author_display: 'BnE7..NRGF',
-            created_at: 'Nov 26, 2021 1:03PM',
-            created_ago: '2 hours ago',
-            timestamp: 1637932864,
-        },
-        {
-            topic: '',
-            content: 'Just setting up my Solana twttr',
-            author_display: 'B1Af..wtRN',
-            created_at: 'Nov 26, 2021 1:03PM',
-            created_ago: '2 days ago',
-            timestamp: 1637932864,
-        },
-    ]
+    const { program } = useWorkspace()
+    const tweets = await program.value.account.tweet.all();
+    return tweets
 }
