8d5930e0bc86e58378072275523a41b8484cca02
Loris Leiva <loris.leiva@gmail.com>
2021-12-05 13:05:03 +0000

    The right commitment

--- app/src/composables/useWorkspace.js
+++ app/src/composables/useWorkspace.js
@@ -4,6 +4,8 @@ import { Connection, PublicKey } from '@solana/web3.js'
 import { Provider, Program } from '@project-serum/anchor'
 import idl from '../../../target/idl/solana_twitter.json'
 
+const preflightCommitment = 'processed'
+const commitment = 'processed'
 const programID = new PublicKey(idl.metadata.address)
 const workspaceSymbol = Symbol()
 
@@ -11,8 +13,8 @@ export const useWorkspace = () => inject(workspaceSymbol)
 
 export const initWorkspace = () => {
     const wallet = useAnchorWallet()
-    const connection = new Connection('http://127.0.0.1:8899')
-    const provider = computed(() => new Provider(connection, wallet.value))
+    const connection = new Connection('http://127.0.0.1:8899', commitment)
+    const provider = computed(() => new Provider(connection, wallet.value, { preflightCommitment, commitment }))
     const program = computed(() => new Program(idl, programID, provider.value))
 
     provide(workspaceSymbol, {
