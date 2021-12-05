82d821bd70987616ab65cd6aeeb975c94f586df3
Loris Leiva <loris.leiva@gmail.com>
2021-12-05 13:04:41 +0000

    Sending tweets

--- app/src/api/send-tweet.js
+++ app/src/api/send-tweet.js
@@ -1,10 +1,20 @@
+import { web3 } from '@project-serum/anchor'
+import { useWorkspace } from '@/composables'
+import { Tweet } from '@/models'
+
 export const sendTweet = async (topic, content) => {
-    return {
-        topic,
-        content,
-        author_display: 'B1Af..wtRN',
-        created_at: 'Nov 26, 2021 1:03PM',
-        created_ago: 'just now',
-        timestamp: 1637932868,
-    }
+    const { wallet, program } = useWorkspace()
+    const tweet = web3.Keypair.generate()
+
+    await program.value.rpc.sendTweet(topic, content, {
+        accounts: {
+            author: wallet.value.publicKey,
+            tweet: tweet.publicKey,
+            systemProgram: web3.SystemProgram.programId,
+        },
+        signers: [tweet]
+    })
+
+    const tweetAccount = await program.value.account.tweet.fetch(tweet.publicKey)
+    return new Tweet(tweet.publicKey, tweetAccount)
 }
