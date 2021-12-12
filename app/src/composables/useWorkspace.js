bcc5ceba79557d1e732536c70419385f856a4501
Loris Leiva <loris.leiva@gmail.com>
2021-12-12 10:50:01 +0000

    Copying the IDL file

--- app/src/composables/useWorkspace.js
+++ app/src/composables/useWorkspace.js
@@ -2,7 +2,7 @@ import { inject, provide, computed } from 'vue'
 import { useAnchorWallet } from '@solana/wallet-adapter-vue'
 import { Connection, PublicKey } from '@solana/web3.js'
 import { Provider, Program } from '@project-serum/anchor'
-import idl from '../../../target/idl/solana_twitter.json'
+import idl from '@/idl/solana_twitter.json'
 
 const preflightCommitment = 'processed'
 const commitment = 'processed'
