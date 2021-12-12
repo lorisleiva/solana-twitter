64a68a30bf3f4720e73cf34a9882b4139b34fde8
Loris Leiva <loris.leiva@gmail.com>
2021-12-12 11:08:10 +0000

    Multiple environments in the frontend

--- app/src/composables/useWorkspace.js
+++ app/src/composables/useWorkspace.js
@@ -4,6 +4,7 @@ import { Connection, PublicKey } from '@solana/web3.js'
 import { Provider, Program } from '@project-serum/anchor'
 import idl from '@/idl/solana_twitter.json'
 
+const clusterUrl = process.env.VUE_APP_CLUSTER_URL
 const preflightCommitment = 'processed'
 const commitment = 'processed'
 const programID = new PublicKey(idl.metadata.address)
@@ -13,7 +14,7 @@ export const useWorkspace = () => inject(workspaceSymbol)
 
 export const initWorkspace = () => {
     const wallet = useAnchorWallet()
-    const connection = new Connection('https://api.devnet.solana.com', commitment)
+    const connection = new Connection(clusterUrl, commitment)
     const provider = computed(() => new Provider(connection, wallet.value, { preflightCommitment, commitment }))
     const program = computed(() => new Program(idl, programID, provider.value))
 
