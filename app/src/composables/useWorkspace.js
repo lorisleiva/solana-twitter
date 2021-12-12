6ffe2c0daaceac4f2e96790a31d59193d24a7ab6
Loris Leiva <loris.leiva@gmail.com>
2021-12-12 10:18:36 +0000

    Deploying the program

--- app/src/composables/useWorkspace.js
+++ app/src/composables/useWorkspace.js
@@ -13,7 +13,7 @@ export const useWorkspace = () => inject(workspaceSymbol)
 
 export const initWorkspace = () => {
     const wallet = useAnchorWallet()
-    const connection = new Connection('http://127.0.0.1:8899', commitment)
+    const connection = new Connection('https://api.devnet.solana.com', commitment)
     const provider = computed(() => new Provider(connection, wallet.value, { preflightCommitment, commitment }))
     const program = computed(() => new Program(idl, programID, provider.value))
 
